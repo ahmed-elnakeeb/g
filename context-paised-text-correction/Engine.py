b515cf969b02a40f0b0085023e931e2921592393
ahmed elnakeeb <ahmedelnakeeb2016@outlook.com>
2020-07-25 14:47:34 +0200

    engine

--- context-paised-text-correction/Engine.py
+++ context-paised-text-correction/Engine.py
@@ -1,44 +1,24 @@
+from spellchecker import SpellChecker
+from tqdm import tqdm
+from tqdm import trange
+
 class engin:
-    def __init__(self,studentanswer:str,modelanswers:list,keywords:list):
+    def __init__(self,studentanswer:str,modelanswers:list,keywords:list,trng:int):
         self.student_Answer=studentanswer
         self.model_Answers=modelanswers
         self.Keywords=keywords
+        self.trng=trng
 
-    def _prepare_answers(self):
-        """
-        toknise student and model answers
-        """
-        pass
-
-    def _load_dict(self,location):
-        """
-        load dect to add to possible correction even it is not in keywords
-        """
-        pass
-
-    def _load_json_from_file(self,location:str):
-        with open('data.json') as f:
-            data = json.load(f)
-        #jso=json.dumps(data)
-        return data
-    def _export_json_file(self,file,location):
-        with open('new.json', 'w') as json_file:
-            json.dump(file, json_file,indent=4,sort_keys=True)
-
-    def _correct(self):
-        """
-        for each wrong word in student answer see if a correction nomait is in keywords or at least correct it to some thing related to the context 
-        """
-        pass
-
-    def _nomait(self,word:str):
-        """
-        get the most likly correction of certin word 
-        """
-        pass
-
-    def get_degree(self):
-        """
-        some math to convert our data into numbers
-        """
-        pass
+    def correct(self)->str:
+        spell=SpellChecker()
+        corrected_text=""
+        #toknize
+        words=spell._tokenizer(self.student_Answer)
+        # get keywords ready 
+        for x in trange (1,self.trng):
+            spell.word_frequency.load_text_file("keywords.txt")
+        for x in trange(1,100):
+            spell.word_frequency.load_words(self.Keywords)
+        for word in tqdm(words):
+            corrected_text+=spell.correction(word)+" "
+        return corrected_text
--- context-paised-text-correction/cptc.py
+++ context-paised-text-correction/cptc.py
@@ -23,18 +23,14 @@ nlp not effcient neither am i
 how to collect the degree 
 uncertinty
 """
-
 from my_JSON import my_JSON
 from Engine import engin
 
 
-db=my_JSON()
-x=db.load_json_from_file("clarify.json")
 
-with open("keywords.txt", 'w') as json_file:
-    for key in x:
-        words=x[key][1]
-        for word in words:
-            json_file.write(word+"\n")
+#db=my_JSON()
+#x=db.load_json_from_file("clarify.json")
 
-c=engin("i like red . i do not like blue",["i like red . i do not like blue","i hate blue but not red"],["like_red","hate_blue"])
+
+c=engin("the light is faster than lightneng ",["i like red . i do not like blue","i hate blue but not red"],["red","blue"],10)
+print(c.correct())
